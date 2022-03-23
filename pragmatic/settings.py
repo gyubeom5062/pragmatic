a4594fb088049eadeebdc1863664dc53975a8951
gyubeom5062 <gyubeom5062@gmail.com>
2022-03-23 16:17:47 +0900

    django course 11 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -139,6 +139,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# static 파일을 한 곳으로 관리하기 위해 root 경로 설정
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
